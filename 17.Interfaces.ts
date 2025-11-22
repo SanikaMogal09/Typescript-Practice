75cd59d3bea5d899cbeaed197884976b3dec52c2
Sanika Mogal <sanikamogal925.com>
2025-11-23 00:00:33 +0530

    Renamed the files

---
+++ 17.Interfaces.ts
@@ -0,0 +1,6 @@
+// Interface
+// Interface is a way to define a contract for the shape of an object.
+// It specifies the properties and type that an object must have
+// Interfaces are a powerful tool for enforcing a certain structure in your code
+
+// they can be also used to define the shape of functions and classes
